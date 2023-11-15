4fc021620eed84e1f3936cac55c4ff4781a14b06
jack.ooi <jack.ooi@bp.com>
2023-11-15 11:55:20 +0800

    Completed Challenge 5,6,7,8

--- Challenges.cs
+++ Challenges.cs
@@ -1,7 +1,11 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using FluentAssertions;
 using Microsoft.VisualBasic;
+using Xunit.Sdk;
 
 namespace consolidation_csharp_lesson_5;
+
 public static class Challenges
 {
     public static int[] GetNumbersOver50V1(int[] numbers)
@@ -22,6 +26,7 @@ public static class Challenges
         return numbers.Where(nums => nums > 50).ToArray<int>();
     }
 
+    //Stil got issue with the unit test, need to check again the logic
     public static string[] GetWordsUnder5CharacterV1(string[] words)
     {
         var newList = new List<string>();
@@ -34,6 +39,7 @@ public static class Challenges
         }
         return newList.ToArray();
     }
+
     public static string[] GetWordsUnder5CharacterV2(string[] words)
     {
         var wordArray = words.Where(word => word.Length < 5).ToArray();
@@ -42,22 +48,46 @@ public static class Challenges
 
     public static string[] GetNamesV1(List<People> people)
     {
-        throw new NotImplementedException();
+        if (people == null)
+        {
+            throw new ArgumentNullException(nameof(people));
+        }
+
+        var namesArray = new List<string>();
+
+        foreach (var person in people)
+        {
+            namesArray.Add(person.Name);
+        }
+
+        return namesArray.ToArray();
     }
 
     public static string[] GetNamesV2(List<People> people)
     {
-        throw new NotImplementedException();
+        var namesArray = people.Select(person => person?.Name).ToArray();
+        return namesArray;
     }
 
     public static int[] DoubleNumbersV1(int[] numbers)
     {
-        throw new NotImplementedException();
+        if (numbers == null)
+        {
+            throw new ArgumentNullException(nameof(numbers));
+        }
+
+        var doubleArray = new List<int>();
+        foreach (var number in numbers)
+        {
+            doubleArray.Add(number * 2);
+        }
+        return doubleArray.ToArray();
     }
 
     public static int[] DoubleNumbersV2(int[] numbers)
     {
-        throw new NotImplementedException();
+        int[] doubleArray = numbers.Select(number => number * 2).ToArray();
+        return doubleArray;
     }
 
     public static string[] StringSplit(string stringToSplit)
@@ -67,7 +97,18 @@ public static class Challenges
 
     public static string StringJoin(string separator, string[] stringsToJoin)
     {
-        throw new NotImplementedException();
+        if (stringsToJoin == null)
+        {
+            throw new ArgumentNullException(nameof(stringsToJoin));
+        }
+
+        var substrings = new List<string>();
+        int startIndex = 0;
+
+        while (startIndex < substrings.Count)
+        {
+
+        }
     }
 
     public static List<T> ListReverse<T>(List<T> list)
